566891299fa5153d4bae23fc9bb716bb32fa0b0b
yuki yamada <yuuki@yukisbook.local>
2018-03-25 20:55:45 -0300

    Editted PositiveWordList

--- TwitterLite/src/PositiveWordList.java
+++ TwitterLite/src/PositiveWordList.java
@@ -2,13 +2,35 @@ import java.util.ArrayList;
 
 public class PositiveWordList extends ArrayList<String> {
 
+    //test
+    public final static String[]testWords = {"Good", "Great", "Excellent"};
+
+    public PositiveWordList(){
+
+    }
+
+    //test
+    public void addTestWords(){
+        addPositiveWords(testWords);
+    }
+
     /**
      * Adds a positive word to this
      * @param word
      */
-    public void addPosWord(String posWord){
+    public void addAPositiveWord(String posWord){
         add(posWord);
     }
+
+    public void addPositiveWords(String[]posWords){
+        for(String word: posWords){
+            add(word);
+        }
+    }
+
+
+
+
 
     /**
      * Checks if this PositiveWordList contains the word given
