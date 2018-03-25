0b67303791e8e29de0ea2e1a5e6482a37c5a41f1
yuki yamada <yuuki@yukisbook.local>
2018-03-25 18:46:38 -0300

    Editted UserDatabase

--- TwitterLite/src/UserDatabase.java
+++ TwitterLite/src/UserDatabase.java
@@ -1,14 +1,57 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class UserDatabase {
     private int numOfUsers;
     private int numOfGroups;
     private int numOfTweets;
     private int numOfPositiveMsg;
+    private ArrayList<Tweet> allTweets;
+    private HashMap<String, UserComponent> allUsersAndGroupsMap;
 
-    public void addUser() {
+
+    /**
+     * add an user to a HashMap
+     * @param id
+     * @param user
+     */
+    public void addUser(String id, UserComponent user) {
+        allUsersAndGroupsMap.put(id, user);
         numOfUsers++;
     }
 
-    public void addGroup() {
+    /**
+     * Adds a group to a hash map
+     * @param id
+     * @param group
+     */
+    public void addGroup(String id, UserComponent group) {
+        allUsersAndGroupsMap.put(id, group);
         numOfGroups++;
+    }
+
+
+    public void addTweet(Tweet tweet){
+        allTweets.add(tweet);
+        numOfTweets++;
+    }
+
+    
+
+    //getters
+    public int getNumOfUsers() {
+        return numOfUsers;
+    }
+
+    public int getNumOfGroups() {
+        return numOfGroups;
+    }
+
+    public int getNumOfTweets() {
+        return numOfTweets;
+    }
+
+    public int getNumOfPositiveMsg() {
+        return numOfPositiveMsg;
     }
 }
