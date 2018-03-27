542a780c6b0fa6981de4c8ccd0441b24db3ebaf8
yuki yamada <yuuki@yukisbook.local>
2018-03-27 15:27:04 -0300

    added some positive words to PositiveWordList

--- TwitterLite/src/PositiveWordList.java
+++ TwitterLite/src/PositiveWordList.java
@@ -3,48 +3,46 @@ import java.util.ArrayList;
 public class PositiveWordList extends ArrayList<String> {
 
     //test
-    public final static String[]testWords = {"Good", "Great", "Excellent"};
+    public final static String[] testWords = {"Good", "Great", "Excellent", "Easy", "Happy", "Lucky"};
 
     /**
      * Constructor
      */
-    public PositiveWordList(){
+    public PositiveWordList() {
         addTestWords();//add the test words to this
     }
 
     //test
-    public void addTestWords(){
+    public void addTestWords() {
         addPositiveWords(testWords);
     }
 
     /**
      * Adds a positive word to this
+     *
      * @param
      */
-    public void addAPositiveWord(String posWord){
+    public void addAPositiveWord(String posWord) {
         add(posWord);
     }
 
-    public void addPositiveWords(String[]posWords){
-        for(String word: posWords){
+    public void addPositiveWords(String[] posWords) {
+        for (String word : posWords) {
             add(word);
         }
     }
 
 
-
-
-
     /**
      * Checks if this PositiveWordList contains the word given
+     *
      * @param
      * @return
      */
-    public boolean isPositive(String msg){
-        //System.out.println(msg);
-        for(String stored: this) {
-            System.out.println(stored + "\n");
-            return msg.toLowerCase().contains(stored.toLowerCase());
+    public boolean isPositive(String msg) {
+        for (String stored : this) {
+            if (msg.toLowerCase().contains(stored.toLowerCase()))
+                return true;
         }
         return false;
     }
