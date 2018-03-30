7de7501e1f2c999f87a1e8469cdfc956de3ea02e
Aleix Molla Marco <amolla@upei.ca>
2018-03-29 21:16:35 -0300

    visotor

--- TwitterLite/src/Visitor.java
+++ TwitterLite/src/Visitor.java
@@ -1,2 +1,6 @@
 public interface Visitor {
+    public void visit(User user);
+    public void visit(UserGroup group);
+    public void visit(AdminController adminController);
+    public void visit(UserPanelController userPanelController);
 }
