b0053309cf432c1ded35f38f1520c2e6dcd68902
Aleix Molla Marco <amolla@upei.ca>
2018-03-30 21:26:01 -0300

    delete

--- TwitterLiteOld/src/Subject.java
+++
@@ -1,5 +0,0 @@
-public interface Subject {
-    public abstract void notifyObservers();
-    public abstract void registerObserver(Observer observer);
-    public abstract void removeObserver(Observer observer);
-}
